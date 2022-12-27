9bd214712472e9e2cd456a1f1a9afb827e6a92f7
yhw0104 <yhw0104@naver.com>
2022-12-27 17:15:12 +0900

    4th commit

--- inflearn-spring-introduce/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ inflearn-spring-introduce/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
-    Optional<Member> findByName(String name);    // Optional -> null값을 그대로 반환하지 않고 optional로 감싸서 반환
+    Optional<Member> findByName(String name);    // Optional -> 이름이 없어 null값으로 나올 경우 null값을 그대로 반환하지 않고 optional로 감싸서 반환
     List<Member> findAll();
 }
--- inflearn-spring-introduce/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ inflearn-spring-introduce/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,33 +2,35 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static Map<Long, Member> store = new HashMap<>();   // 회원정보 메모리 저장할 곳
+
+    private static long sequence = 0L;  // sequence -> key값을 생성해주는 변수
 
     @Override
     public Member save(Member member) {
-        return null;
+        member.setId(++sequence);   // sequence 값을 올려 만들 때마다 id 값 생성
+        store.put(member.getId(), member);  // store에 id와  이름을 저장
+        return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));  // id 값이 null이여도 optional로 감싸서 반환 -> Optional.ofNullable()
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()     // 람다식.루프
+                .filter(member -> member.getName().equals(name))    // 루프를 돌려 name이 같은 member를 찾는다.
+                .findAny();     // 파라미터와 같은 name 값을 찾으면 그 값을 반환
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
     }
 }
