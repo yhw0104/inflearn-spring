dd5c780c69161410500c818d47f79db81b9b7c8d
yhw0104 <yhw0104@naver.com>
2022-12-29 18:37:29 +0900

    9th commit

--- inflearn-spring-introduce/src/main/java/hello/hellospring/controller/MemberController.java
+++ inflearn-spring-introduce/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,14 +2,16 @@ package hello.hellospring.controller;
 
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public MemberController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
     }
 }
