5b225dc573d72ad40dcdfd352983ac88e793b568
yhw0104 <yhw0104@naver.com>
2023-01-15 21:35:07 +0900

    3rd commit

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,11 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
+
+    //의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음
+    // MemberServiceImpl 에서 repository를 의존할 때 MemberRepository인터페이스(추상화)와 그 구현체인 MemoryMemberRepository(구체화)도 의존하고 있다.
+    // --> DIP 위반
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
