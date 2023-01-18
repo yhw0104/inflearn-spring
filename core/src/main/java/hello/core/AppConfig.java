093f4afc09fb6004e2a316f9b63263a14d44e2ff
yhw0104 <yhw0104@naver.com>
2023-01-18 17:07:51 +0900

    8th commit

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -12,10 +12,18 @@ public class AppConfig {
 
     // 생성자 주입
     public MemberService memberService() {
-         return new MemberServiceImpl(new MemoryMemberRepository());
+         return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
