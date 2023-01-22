d70db90f5b26bb984e0413bc31c8b6bdbcfe61a5
yhw0104 <yhw0104@naver.com>
2023-01-22 21:26:40 +0900

    15th commit

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,17 +1,17 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class XmlAppContext {
     @Test
     void xmlAppContext() {
-        ApplicationContext ac = new GenericApplicationContext("appConfig.xml");
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
     }
-
 }
