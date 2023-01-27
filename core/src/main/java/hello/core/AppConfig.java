796440d94db3328f51eb73ddb9b52b54134bd80c
yhw0104 <yhw0104@naver.com>
2023-01-27 16:39:24 +0900

    20th commit

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService() -> new MemoryMemberRepository()
+    // @Bean orderService() -> new MemoryMemberRepository(), new RateDiscountPolicy()
+    //이러면 MemoryMemberRepository가 각각 두번 생성되어서 싱글톤 패턴이 깨질거라고 생각한다.
+    // 하지만 ConfigurationSingletonTest를 보면 세개의 memberRepository가 같은 인스턴스를 가지고 있다.
+
+    // sout 예상
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository (memberRepository 빈 생성 할 때 출력)
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    // sout 결과
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
     @Bean
     // 생성자 주입
     public MemberService memberService() {
-         return new MemberServiceImpl(memberRepository());
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
