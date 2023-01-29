211a5d407f91c0a62aced4a70784d2aefc464852
yhw0104 <yhw0104@naver.com>
2023-01-29 18:07:19 +0900

    22th commit

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         //AppConfig 파일이 실행되면 동시에 빈이 생성 되기 때문에, 이를 방지하기 위해 설정해둔다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
