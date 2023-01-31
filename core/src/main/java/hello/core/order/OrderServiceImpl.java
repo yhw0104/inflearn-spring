27e969cd1b988b7785445f37647fabfbc5021521
yhw0104 <yhw0104@naver.com>
2023-01-31 17:41:19 +0900

    25th commit

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,10 +5,12 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor    //( final이 붙은 값을 파라미터로 받는 생성자를 만듬) --> 단일 생성자이면 @Autowired가 필요 없으므로 자동으로 di 해줌
 public class OrderServiceImpl implements  OrderService{
 
     //회원 레파지토리
@@ -16,16 +18,12 @@ public class OrderServiceImpl implements  OrderService{
     //고정 할인 정책
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-
-        //memberRepository와 discountPolicy가 의존하는 구현체에가 들어간다. memberRepository --> memoryMemberRepository , discountPolicy --> RateDiscountPolicy
-        this.memberRepository = memberRepository;
-        System.out.println("memberRepository = " + memberRepository);
-        this.discountPolicy = discountPolicy;
-        System.out.println("discountPolicy = " + discountPolicy);
-
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        //memberRepository와 discountPolicy가 의존하는 구현체에가 들어간다. memberRepository --> memoryMemberRepository , discountPolicy --> RateDiscountPolicy
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
