cabb60872bffb42a78af4d16ac05e3a3411e588e
yhw0104 <yhw0104@naver.com>
2023-02-01 16:31:30 +0900

    26th commit

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
@@ -7,10 +8,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor    //( final이 붙은 값을 파라미터로 받는 생성자를 만듬) --> 단일 생성자이면 @Autowired가 필요 없으므로 자동으로 di 해줌
+//@RequiredArgsConstructor    //( final이 붙은 값을 파라미터로 받는 생성자를 만듬) --> 단일 생성자이면 @Autowired가 필요 없으므로 자동으로 di 해줌
 public class OrderServiceImpl implements  OrderService{
 
     //회원 레파지토리
@@ -18,12 +20,12 @@ public class OrderServiceImpl implements  OrderService{
     //고정 할인 정책
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        //memberRepository와 discountPolicy가 의존하는 구현체에가 들어간다. memberRepository --> memoryMemberRepository , discountPolicy --> RateDiscountPolicy
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        //memberRepository와 discountPolicy가 의존하는 구현체에가 들어간다. memberRepository --> memoryMemberRepository , discountPolicy --> RateDiscountPolicy
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
