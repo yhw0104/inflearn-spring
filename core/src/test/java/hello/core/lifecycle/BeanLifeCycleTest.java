67365937e87d242744474ddef2036a6d07d771cd
yhw0104 <yhw0104@naver.com>
2023-02-05 23:41:51 +0900

    27th commit

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+//        ApplicationContext로는 close메서드를 쓸 수 없다
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,48 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작 시 호출하는 메서드
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    //연결된 상태
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출하는 메서드 (안전하게 서비스 연결이 끊김)
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+
+    //의존 주입 다 끝나고 실행됨
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    //서비스 종료시 실행
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
