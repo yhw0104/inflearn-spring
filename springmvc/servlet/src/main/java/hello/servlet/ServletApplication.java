b0f3edae1e7d59d1a58c6a486b52bfc7c59a550a
yhw0104 <yhw0104@naver.com>
2023-02-14 19:58:54 +0900

    2nd commit

--- springmvc/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ springmvc/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	// 자동으로 서블릿을 찾아서 실행(자동등록) --> 해당 어노테이션이 있는 페키지 내의 모든 서블릿 파일
 @SpringBootApplication
 public class ServletApplication {
 
